684a8c8103f777e7b25a36cc96811ef63c44e0a5
Amareashwar <60135401+Amareashwar@users.noreply.github.com>
2020-01-24 08:09:32 +0530

    Add files via upload

---
+++ startupcomanies.js
@@ -0,0 +1,61 @@
+const mysql = require('mysql');
+const express = require('express');
+var app = express();
+const bodyparser = require('body-parser');
+
+app.use(bodyparser.json());
+
+var mysqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Am@r8106',
+    database: 'startup',
+    multipleStatements: true
+});
+
+mysqlConnection.connect((err) => {
+    if (!err)
+        console.log('DataBase connection is done.');
+    else
+        console.log('DB connection was  failed \n Error : ' + JSON.stringify(err, undefined, 2));
+});
+
+
+app.listen(3000, () => console.log('Express server is runnig at port no : 3000'));
+
+
+//Get all employees
+app.get('/startupcompanies', (req, res) => {
+    mysqlConnection.query('SELECT * FROM startupcompanies', (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    })
+});
+
+//Get an employees
+app.get('/startupcompanies/idstartupcompanies:', (req, res) => {
+    mysqlConnection.query('SELECT * FROM startupcompanies WHERE idstartupcompanies= ?', [req.params.id], (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    })
+});
+//Insert an employees
+app.post('/employees', (req, res) => {
+    let emp = req.body;
+    var sql = "SET idstartupcompanies = ?;SET @Name = ?;SET @country = ?; \
+    CALL EmployeeAddOrEdit(@idstartupcompanies,@Name,@country);";
+    mysqlConnection.query(sql, [emp.idstartupcompanies, emp.Name, emp.country], (err, rows, fields) => {
+        if (!err)
+            rows.forEach(element => {
+                if(element.constructor == Array)
+                res.send('Inserted employee id : '+element[0].EmpID);
+            });
+        else
+            console.log(err);
+    })
+});
+
